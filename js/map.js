80fbb75fd08f10fddec8d330c536a31e57c4e62f
suubinkim <0vbbbv0@gmail.com>
2022-02-01 19:54:13 +0900

    feat : 오프라인 가게 지도에 나타내기

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 let geocoder;
 let Curlocation;
+let Splitlocation;
 
 $(document).ready(function () {
     //가게 리스트 리셋
@@ -14,6 +15,7 @@ $(document).ready(function () {
 })
 
 function CurrentLocation() {
+
     if (navigator.geolocation) {
 
         // GeoLocation을 이용해서 접속 위치를 얻어옵니다
@@ -27,6 +29,8 @@ function CurrentLocation() {
 
             // 마커와 인포윈도우를 표시합니다
             displayMarker(locPosition, message);
+            //현재 위치 주소 가져오기
+            searchAddrFromCoords(locPosition, displayCenter);
         });
 
     } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
@@ -35,7 +39,9 @@ function CurrentLocation() {
             message = 'geolocation을 사용할수 없어요..'
 
         displayMarker(locPosition, message);
+        searchAddrFromCoords(locPosition, displayCenter);
     }
+
 }
 
 // 지도에 마커와 인포윈도우를 표시하는 함수입니다
@@ -91,8 +97,73 @@ function displayCenterInfo(result, status) {
                 break;
             }
         }
-        let locationArr = Curlocation.split(" ");
-        let location = locationArr[0] + " " + locationArr[1]
-        aroundShop(location);
     }
 }
+
+function displayCenter(result, status) {
+    console.log(result)
+    if (status === kakao.maps.services.Status.OK) {
+
+        for (let i = 0; i < result.length; i++) {
+            // 행정동의 region_type 값은 'H' 이므로
+            if (result[i].region_type === 'H') {
+                Curlocation = result[i].address_name;
+                break;
+            }
+        }
+        let locationArr = Curlocation.split(" ");
+        Splitlocation = locationArr[0] + " " + locationArr[1]
+        aroundShop(Splitlocation)
+    }
+}
+
+function showOnMap(data) {
+    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+    var bounds = new kakao.maps.LatLngBounds();
+
+    for (let i = 0; i < data['display']; i++) {
+        let title = data['items'][i]['title'];
+        let address = data['items'][i]['address'];
+
+        // 주소-좌표 변환 객체를 생성합니다
+        var geocoder = new kakao.maps.services.Geocoder();
+
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch(address, function (result, status) {
+
+            // 정상적으로 검색이 완료됐으면
+            if (status === kakao.maps.services.Status.OK) {
+
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
+                let marker = new kakao.maps.Marker({position: coords});
+                marker.setMap(map);
+
+                // LatLngBounds 객체에 좌표를 추가합니다
+                bounds.extend(coords);
+
+                // var marker = new kakao.maps.Marker({
+                //     map: map,
+                //     position: coords
+                // });
+
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: `<div style="width:150px;text-align:center;padding:6px 0;">${title}</div>`
+                });
+                infowindow.open(map, marker);
+
+                // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
+                // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
+                map.setBounds(bounds);
+
+                // // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                // map.setCenter(coords);
+            }
+        });
+    }
+}
+
+
